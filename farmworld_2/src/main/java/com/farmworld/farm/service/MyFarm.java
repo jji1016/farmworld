c5e0d834c6696a00b7731d2b3b8e3c6551ca3814
sebeomlee <leetmdrnjs77@gmail.com>
2024-01-08 15:58:02 +0900

    pageDTO, Criteria추가, farm패키치 수정

--- farmworld_2/src/main/java/com/farmworld/farm/service/MyFarm.java
+++ farmworld_2/src/main/java/com/farmworld/farm/service/MyFarm.java
@@ -2,10 +2,11 @@ package com.farmworld.farm.service;
 
 import java.util.List;
 
+import com.farmworld.all.domain.Criteria;
 import com.farmworld.all.service.Generic;
 import com.farmworld.farm.domain.MyFarmVO;
 
-public interface MyFarm extends Generic<VO, K> {
+public interface MyFarm extends Generic<MyFarmVO, Integer> {
 	
 	public List<MyFarmVO> farmAll();
 	
